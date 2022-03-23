99bab9381835512dbcbc36f70377a72665c9916c
JeffAllison <itsjallison@gmail.com>
2022-03-23 12:29:46 -0700

    made edits to clear eslint errors

--- fetch-utils.js
+++ fetch-utils.js
@@ -17,53 +17,53 @@ The games are stored in the database using this data model:
 }
 */
 
-export async function createGame(game){
-    // create a single new game in the games table using the above object
-    
-    return checkError(response);
+export async function createGame(game) {
+  // create a single new game in the games table using the above object
+
+  return checkError(response);
 }
 
 export async function getGames() {
-    // select all games from the games table
+  // select all games from the games table
 
-    return checkError(response);    
+  return checkError(response);
 }
 
 export async function getUser() {
-    return client.auth.session();
+  return client.auth.session();
 }
 
 
 export async function checkAuth() {
-    const user = await getUser();
+  const user = await getUser();
 
-    if (!user) location.replace('../'); 
+  if (!user) location.replace('../');
 }
 
 export async function redirectToGames() {
-    if (await getUser()) {
-        location.replace('./games');
-    }
+  if (await getUser()) {
+    location.replace('./games');
+  }
 }
 
-export async function signupUser(email, password){
-    const response = await client.auth.signUp({ email, password });
-    
-    return response.user;
+export async function signupUser(email, password) {
+  const response = await client.auth.signUp({ email, password });
+
+  return response.user;
 }
 
-export async function signInUser(email, password){
-    const response = await client.auth.signIn({ email, password });
+export async function signInUser(email, password) {
+  const response = await client.auth.signIn({ email, password });
 
-    return response.user;
+  return response.user;
 }
 
 export async function logout() {
-    await client.auth.signOut();
+  await client.auth.signOut();
 
-    return window.location.href = '../';
+  return window.location.href = '../';
 }
 
 function checkError({ data, error }) {
-    return error ? console.error(error) : data;
+  return error ? console.error(error) : data;
 }
